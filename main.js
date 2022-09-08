532f1770bd08c5f5174a2cce8c2036461e4235b7
Martinchoo23 <tincho2000k2@hotmail.com>
2022-09-08 10:15:23 -1100

    function

--- main.js
+++ main.js
@@ -7,18 +7,18 @@ while (securityWord !== 'XJA0EZ') {
     alert('¡Bien! puedes continuar...')
 }
 
+function compra() {
+    let precio = 0;
+    let total = 0;
+    let producto;
+    let cantidad = 0;
+    let seguirComprando = false;
 
-let precio = 0;
-let producto;
-let cantidad = 0;
-let total = 0;
-let seguirComprando = false;
-
-do {
-    producto = prompt('¿Qué articulo deseas comprar? (tipea el número correspondiente al producto)' + "\n1) Proteína WHEY 1kg \n2) Proteína WHEY 3kg \n3) Proteína WHEY 5kg")    
-
-
-    switch (producto) {
+    do {
+        producto = prompt('¿Qué articulo deseas comprar? (tipea el número correspondiente al producto)' + "\n1) Proteína WHEY 1kg \n2) Proteína WHEY 3kg \n3) Proteína WHEY 5kg")
+    
+    
+        switch (producto) {
         case "1":
             precio = 1500
             confirm('El precio de la proteína de 1kg es: $1.500. ¿Deseas agregarlo al carrito?')
@@ -34,13 +34,15 @@ do {
         default:
             alert('El número que ingresaste no corresponde a ningún producto.')
     }
+    
     cantidad = Number(prompt('¿Cuántos quieres?'))
-
+    
     total += precio * cantidad;
+    
     seguirComprando = confirm('¿Quieres seguir comprando?')
+    } while (seguirComprando)
 
-} while (seguirComprando)
-
-alert('Muchas gracias por elegirnos! el total de tu compra es: ' + total + "$");
-
+alert('Muchas gracias por elegirnos! el total de tu compra es de: ' + total + "$");
+}
+compra();
 
